9511294b373042ae1565cb50ff77341c7b25e569
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-01-16 00:37:22 -0600

    implement firebase and connect store to compoenents

--- src/component/projects/CreateProject.js
+++ src/component/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createProject } from "../../store/actions/projectActions";
 
 export class CreateProject extends Component {
   state = {
@@ -13,7 +15,7 @@ export class CreateProject extends Component {
   };
   handleOnSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.createProject(this.state);
   };
   render() {
     return (
@@ -43,4 +45,13 @@ export class CreateProject extends Component {
   }
 }
 
-export default CreateProject;
+const mapDispatchToProps = dispatch => {
+  return {
+    createProject: project => dispatch(createProject(project))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CreateProject);
---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { deflate } from "zlib";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCysEoQaJH7XgHZLf4gxqB9U7erdgw4GJo",
+  authDomain: "react-project-app-7651a.firebaseapp.com",
+  databaseURL: "https://react-project-app-7651a.firebaseio.com",
+  projectId: "react-project-app-7651a",
+  storageBucket: "react-project-app-7651a.appspot.com",
+  messagingSenderId: "664275467730"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebae;
